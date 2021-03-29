bf798bb9226bbd55e7acea4f0773e88e0ecdb073
Apresh Agarwal <apreshagarwal@outlook.com>
2021-03-30 02:57:04 +0530

    1. Created a Main class 2. Created Employee Class 3. Added Create Employee functionality

---
+++ src/Employee.java
@@ -0,0 +1,152 @@
+import java.util.*;
+
+public class Employee {
+    private static final ArrayList<Employee> employees;
+    private final static int AGE_MIN_LIMIT;
+    private final static int AGE_MAX_LIMIT;
+    private final static int MAX_REGISTRATION_LIMIT;
+    private static int LAST_REGISTERED_ID;
+    private final static String COMPANY_CODE;
+    private static final HashMap<String, Integer> designationsAndSalaries;
+    private static final Set<String> namesSet = new HashSet<>();
+    private final String employeeID;
+    private final String name;
+    private final int age;
+    private final String designation;
+    private int salary;
+
+    static {
+        AGE_MIN_LIMIT = 18;
+        AGE_MAX_LIMIT = 60;
+        MAX_REGISTRATION_LIMIT = 10;
+        LAST_REGISTERED_ID = 0;
+        COMPANY_CODE = "WY";
+        employees = new ArrayList<>();
+        designationsAndSalaries = new HashMap<>();
+        designationsAndSalaries.put("Manager", 30000);
+        designationsAndSalaries.put("Developer", 20000);
+        designationsAndSalaries.put("Tester", 25000);
+    }
+
+    public Employee(String employeeID, String name, String designation, int age, int salary) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.designation = designation;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public static boolean registerEmployee() {
+        Scanner STDIN = new Scanner(System.in);
+        int count = employees.size();
+
+        if (count >= MAX_REGISTRATION_LIMIT) {
+            System.out.println("ERROR: Maximum limit reached.");
+            return false;
+        }
+
+        System.out.printf("%nYou can register %d employees.%n", MAX_REGISTRATION_LIMIT - count);
+
+        System.out.println();
+
+        ++LAST_REGISTERED_ID;
+
+        String employeeID = COMPANY_CODE;
+        employeeID += (LAST_REGISTERED_ID < 10) ? ("0" + LAST_REGISTERED_ID) : LAST_REGISTERED_ID;
+
+        System.out.print("Please enter the Employee's Full Name: ");
+        String name;
+        while (true) {
+            name = STDIN.nextLine();
+            if (!name.matches("(\\p{Upper}(\\p{Lower}+\\s?)){2,3}")) {
+                System.out.println("ERROR: INVALID NAME");
+                System.out.println("The name should:");
+                System.out.println("-> contain at least a first name and a last name (middle name is optional)");
+                System.out.println("-> at max 3 words");
+                System.out.println("-> not contain special characters or numbers (alphabets only)");
+                System.out.println("-> not begin with space");
+                System.out.println("-> not contain two consecutive spaces");
+                System.out.println("-> have the first character of each word in upper-case and rest in lower-case");
+                System.out.print("Please try again: ");
+                continue;
+            }
+            if (namesSet.contains(name)) {
+                System.out.printf("The name \"%s\" is already registered. Please try again: ", name);
+                continue;
+            }
+            name = name.trim();
+            namesSet.add(name);
+            break;
+        }
+
+        System.out.print("Please enter the Employee's Age: ");
+        int age;
+        while (true) {
+            try {
+                age = Integer.parseInt(STDIN.nextLine().trim());
+                if (age < AGE_MIN_LIMIT || age > AGE_MAX_LIMIT) {
+                    System.out.println("ERROR: INVALID AGE");
+                    System.out.printf("Employee should be at least %d and at most %d years old. %n", AGE_MIN_LIMIT, AGE_MAX_LIMIT);
+                    System.out.print("Please enter a valid age: ");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.print("ERROR: INVALID AGE");
+                System.out.print("Please enter a valid age: ");
+                continue;
+            }
+            break;
+        }
+
+        System.out.print("Please enter the Employee's Designation (Manager, Developer, Tester): ");
+        String designation = STDIN.nextLine().trim();
+
+        while (!designationsAndSalaries.containsKey(designation)) {
+            System.out.println("ERROR: INVALID DESIGNATION");
+            System.out.println("You can choose only from the following designations:");
+            for (String key: designationsAndSalaries.keySet()) {
+                System.out.printf("-> %s %n", key);
+            }
+            System.out.print("Please enter a valid designation: ");
+            designation = STDIN.nextLine().trim();
+        }
+
+        int salary = designationsAndSalaries.get(designation);
+
+        Employee employee = new Employee(employeeID, name, designation, age, salary);
+        employees.add(employee);
+
+        System.out.println();
+
+        System.out.println("The employee has been registered.");
+
+        if (++count >= MAX_REGISTRATION_LIMIT) return false;
+
+        System.out.printf("Do you want to register another employee? (%d left) (Y/N): ", MAX_REGISTRATION_LIMIT - (count));
+        return true;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public static ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Main {
+    final static String WELCOME_TEXT = "  ___            _                    __  __                                       _     ___         _             \n" +
+            " | __|_ __  _ __| |___ _  _ ___ ___  |  \\/  |__ _ _ _  __ _ __ _ ___ _ __  ___ _ _| |_  / __|_  _ __| |_ ___ _ __  \n" +
+            " | _|| '  \\| '_ \\ / _ \\ || / -_) -_) | |\\/| / _` | ' \\/ _` / _` / -_) '  \\/ -_) ' \\  _| \\__ \\ || (_-<  _/ -_) '  \\ \n" +
+            " |___|_|_|_| .__/_\\___/\\_, \\___\\___| |_|  |_\\__,_|_||_\\__,_\\__, \\___|_|_|_\\___|_||_\\__| |___/\\_, /__/\\__\\___|_|_|_|\n" +
+            "           |_|         |__/                                |___/                             |__/                  \n";
+
+    final static String GITHUB_LINK = "https://github.com/apreshagarwal/Employee-Management-System";
+    final static String LINKEDIN_LINK = "https://linkedin.com/in/apreshagarwal";
+
+    public static void displayMenu() {
+        System.out.println();
+        System.out.println("1. Register New Employees");
+        System.out.println("2. Edit Employee Details");
+        System.out.println("3. Delete Employee");
+        System.out.println("4. Raise Employee's Salary");
+        System.out.println("5. Display All Employees");
+        System.out.println("6. Export Data");
+        System.out.println("7. Exit");
+        System.out.print("Please enter your choice: ");
+    }
+
+    public static void menuOption1(Scanner STDIN) {
+        String choice;
+        boolean isNotDone = Employee.registerEmployee();
+        while (isNotDone) {
+            choice = STDIN.nextLine();
+            switch (choice) {
+                case "Y":
+                case "y":
+                    isNotDone = Employee.registerEmployee();
+                    break;
+
+                case "N":
+                case "n":
+                    isNotDone = false;
+                    break;
+
+                default:
+                    System.out.println("ERROR: Sorry, this is an invalid input. Please try again.");
+                    System.out.print("Please enter your choice: ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner STDIN = new Scanner(System.in);
+
+        System.out.println(WELCOME_TEXT);
+        System.out.printf("GitHub: %s %n", GITHUB_LINK);
+
+        displayMenu();
+        boolean shouldExit = false;
+        String choice;
+
+        MENU: do {
+            choice = STDIN.nextLine();
+            switch (choice) {
+                case "1":
+                    menuOption1(STDIN);
+                    displayMenu();
+                    break;
+
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                    break;
+
+                case "7":
+                    STDIN.close();
+                    System.out.println();
+                    System.out.println("Exiting the application...");
+                    System.out.println("*** Thank you for using the Employee Management System ***");
+                    break MENU;
+
+                default:
+                    System.out.println();
+                    System.out.println("ERROR: Sorry, this is an invalid input. Please try again.");
+                    System.out.print("Please enter your choice: ");
+            }
+        } while (true);
+    }
+}
